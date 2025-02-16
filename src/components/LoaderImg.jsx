1c8c3e2c93f3480f9405ba2356cc79e7fd58b00c
Your Name <progurpreetsingh@gmail.com>
2025-02-16 13:39:41 +0530

    Change loader flashh time

--- src/components/LoaderImg.jsx
+++ src/components/LoaderImg.jsx
@@ -1,6 +1,6 @@
 function LoaderImg() {
   return (
-    <div className="flex justify-center items-center h-screen animate-[flash_1s_ease-in-out_infinite] px-16">
+    <div className="flex justify-center items-center h-screen animate-[flash_0.8s_ease-in-out_infinite] px-16">
       <img src="./QuestionLoader.svg" alt="loader" />
     </div>
   );
