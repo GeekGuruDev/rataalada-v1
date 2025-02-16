932d106af60bc4b9fc2a0cf8b492c183541e2535
Your Name <progurpreetsingh@gmail.com>
2025-02-16 13:30:30 +0530

    Optimize loader image

--- src/components/LoaderImg.jsx
+++ src/components/LoaderImg.jsx
@@ -1,9 +1,7 @@
-import loaderImg from "../assets/QuestionLoader.svg";
-
 function LoaderImg() {
   return (
     <div className="flex justify-center items-center h-screen animate-[flash_1s_ease-in-out_infinite] px-16">
-      <img src={loaderImg} alt="loader" />
+      <img src="./QuestionLoader.svg" alt="loader" />
     </div>
   );
 }
